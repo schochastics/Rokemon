50ce66711d557c6655e55039af8b9ad8e61cb60d
schochastics <david.schoch@uni-konstanz.de>
2017-11-30 09:44:39 +0100

    added

---
+++ R/pokemon.R
@@ -0,0 +1,9 @@
+#' Pokemon Data
+#'
+#' @format A data frame containing various stats from 801 pokemon. Use \code{str(pokemon)}
+#' for more details.
+#'
+#' @references Pokémon data download from [Kaggle](https://www.kaggle.com/rounakbanik/pokemon), originally
+#' scraped from [serebii.net](http://serebii.net/)
+#'
+"pokemon"
