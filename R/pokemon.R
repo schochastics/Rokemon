2dfa393eb20f2052c5d6650c91ba34c3f13e74af
schochastics <david.schoch@uni-konstanz.de>
2017-12-01 19:33:21 +0100

    fixed url issues

--- R/pokemon.R
+++ R/pokemon.R
@@ -3,7 +3,7 @@
 #' @format A data frame containing various stats from 801 pokemon. Use \code{str(pokemon)}
 #' for more details.
 #'
-#' @references Pokémon data download from [Kaggle](https://www.kaggle.com/rounakbanik/pokemon), originally
-#' scraped from [serebii.net](http://serebii.net/)
+#' @references Pokémon data download from Kaggle: \url{https://www.kaggle.com/rounakbanik/pokemon}, originally
+#' scraped from \url{http://serebii.net/}
 #'
 "pokemon"
